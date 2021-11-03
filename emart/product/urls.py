6eeba1e4631fb8fc89b80d92c9c9b6bf8d837d5f
Anshuman <anshumanmaurya111@gmail.com>
2021-11-03 12:18:01 +0530

    Basic Setup and models added

---
+++ emart/product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from django.urls.resolvers import URLPattern
+from product import views
+
+urlpatterns = [
+    path('latest-products/', views.LatestProductList.as_view()),
+]
